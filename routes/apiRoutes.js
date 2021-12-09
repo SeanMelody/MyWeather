37082ac7b22c3993ccced3611e67cfa695459354
Sean Melody <seandmelody@gmail.com>
2021-12-09 13:43:19 -0800

    apiRoutes added

---
+++ routes/apiRoutes.js
@@ -0,0 +1,7 @@
+// const axios = require("axios")
+const router = require("express").Router()
+
+
+
+// Export it all as router
+module.exports = router
--- server.js
+++ server.js
@@ -1,2 +1,22 @@
 // Required Consts!
 const express = require("express")
+const app = express();
+
+
+
+//Port 5005 cause I'm crazy!
+const PORT = process.env.PORT || 5005;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// API Routes start with API and live at apiRoutes.js
+app.use("/api", require("./routes/apiRoutes"))
+
+
+// Let the user know the server is running, and which port.  Yeay!
+app.listen(PORT, () => {
+    console.log(`listening at http://localhost${PORT}`);
+});
